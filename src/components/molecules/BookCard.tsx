54276c23b554281adc973c9ced487806e3b2382d
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-18 21:41:24 +0100

    refactor: enhance BookCard and BookCardText components to include description prop for improved content display

--- src/components/molecules/BookCard.tsx
+++ src/components/molecules/BookCard.tsx
@@ -3,24 +3,30 @@ import BookCardText from "@atoms/BookCardText";
 import Image from "next/image";
 
 export type BookType = {
-  imgSrc: string;
+  img: string;
   title: string;
+  description: string;
   className: string;
 };
 
-const BookCard: FC<BookType> = ({ imgSrc, title, className = "" }) => {
+const BookCard: FC<BookType> = ({
+  img,
+  title,
+  className = "",
+  description,
+}) => {
   return (
     <div
       className={`border-2 border-royal-blue rounded-[0.5rem] overflow-hidden w-48 ${className}`}
     >
       <Image
-        src={imgSrc}
+        src={img}
         alt={title}
         width={192}
         height={208}
         className="border-b-royal-blue border-b-[1px] w-full h-52 object-cover"
       />
-      <BookCardText title={title} />
+      <BookCardText title={title} description={description} />
     </div>
   );
 };
