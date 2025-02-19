7e9b558ac2d8ca1ec3efa6eccadb5f15447fa182
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-19 15:06:12 +0100

    refactor: implement login functionality with form submission and error handling

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import PasswordIcon from "@atoms/PasswordIcon";
@@ -6,8 +7,13 @@ import ForgetPasswordLoginScreen from "@atoms/ForgetPasswordLoginScreen";
 import { useState } from "react";
 import SocialButton from "@atoms/SocialButton";
 import { ScreenType } from "@atoms/TitleUnderSumbitBtnForm";
+import { SubmitHandler } from "react-hook-form";
+import axiosInstance from "@/config/axios";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+  const router = useRouter();
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
@@ -17,6 +23,7 @@ const Login = () => {
   const fieldsList = [
     {
       type: "email",
+      label: "email",
       required: "الرجاء ادخال البريد الالكتروني",
       fieldName: "البريد الإلكتروني",
       pattern: {
@@ -26,6 +33,7 @@ const Login = () => {
     },
     {
       type: passwordVisible ? "text" : "password",
+      label: "password",
       required: "الرجاء ادخال كلمة السر",
       fieldName: "كلمة السر",
       PrefixIcon: PasswordIcon,
@@ -39,6 +47,19 @@ const Login = () => {
     facebook: "تسجيل دخول فيسبوك",
   };
 
+  const onSubmit: SubmitHandler<any> = async (data) => {
+    try {
+      const response = await axiosInstance.post("/auth/login", { ...data });
+
+      Cookies.set("access_token", response.data.accessToken, { expires: 7 });
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+
+      router.push("/");
+    } catch (error: any) {
+      console.log("login error:", error.response?.data || error.message);
+    }
+  };
+
   return (
     <FormTemplate
       inputFields={fieldsList}
@@ -46,6 +67,7 @@ const Login = () => {
       submitBtnClassName="w-full text-xl"
       submitBtnTitle="تسجيل دخول"
       screenType={ScreenType.LOGIN}
+      onSubmit={onSubmit}
     >
       <SocialButton platformsText={platformsText} />
     </FormTemplate>
